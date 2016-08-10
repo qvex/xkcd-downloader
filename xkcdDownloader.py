879ec8180dfff1259ca81919235656e8f2e07963
Whatever <gaurav.singh93@hotmail.com>
2016-08-10 19:22:53 +0530

    Updated comments

--- xkcdDownloader.py
+++ xkcdDownloader.py
@@ -9,32 +9,32 @@ except OSError as e:
 
 while not url.endswith('#'):
 
-    # TODO : Download the page.
+    # Downloading the page
     print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
 
     soup = bs4.BeautifulSoup(res.text, "html.parser")
 
-    # TODO: Find the url of the page.
+    # Get the url of the page.
     comicElem = soup.select('#comic img')
     if comicElem == []:
         print('Could not find comic image.')
     else:
         comicUrl = 'http:' + comicElem[0].get('src')
 
-    # TODO: Download the image.
+    # Downloading the image.
     print('Downloading image %s...' % (comicUrl))
     res = requests.get(comicUrl)
     res.raise_for_status()
 
-    # TODO: Save the image to ./xkcd.
+    # Saving the image to C:\\Users\\localhost\\Desktop\\xkcd\\.
     imageFile = open(os.path.join('C:\\Users\\localhost\\Desktop\\xkcd\\', os.path.basename(comicUrl)), 'wb')
     for chunk in res.iter_content(10000):
         imageFile.write(chunk)
     imageFile.close()
 
-    # TODO: Get the Prev button's url.
+    # Get the Prev button's url.
     prevLink = soup.select('a[rel="prev"]')[0]
     url = 'http://xkcd.com' + prevLink.get('href')
 
